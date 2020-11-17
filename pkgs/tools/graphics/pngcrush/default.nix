c50db4c4f699f329de1f5097190ff6e0cc58834e
Ben Siraphob <bensiraphob@gmail.com>
2020-11-17 18:34:49 +0700

    pngcrush: name -> pname + version

--- pkgs/tools/graphics/pngcrush/default.nix
+++ pkgs/tools/graphics/pngcrush/default.nix
@@ -1,14 +1,15 @@
 { stdenv, fetchurl, libpng }:
 
 stdenv.mkDerivation rec {
-  name = "pngcrush-1.8.13";
+  pname = "pngcrush";
+  version = "1.8.13";
 
   src = fetchurl {
-    url = "mirror://sourceforge/pmt/${name}-nolib.tar.xz";
+    url = "mirror://sourceforge/pmt/${pname}-${version}-nolib.tar.xz";
     sha256 = "0l43c59d6v9l0g07z3q3ywhb8xb3vz74llv3mna0izk9bj6aqkiv";
   };
 
-  makeFlags = [ "CC=cc" "LD=cc" ];      # gcc and/or clang compat
+  makeFlags = [ "CC=cc" "LD=cc" ];
 
   configurePhase = ''
     sed -i s,/usr,$out, Makefile
@@ -16,10 +17,10 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libpng ];
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = "http://pmt.sourceforge.net/pngcrush";
     description = "A PNG optimizer";
-    license = stdenv.lib.licenses.free;
-    platforms = with stdenv.lib.platforms; linux ++ darwin;
+    license = licenses.free;
+    platforms = with platforms; linux ++ darwin;
   };
 }
