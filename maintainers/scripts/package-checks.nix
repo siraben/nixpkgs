488c39c1af80fb2d42d59b87bf81abc08c0ac480
Ben Siraphob <bensiraphob@gmail.com>
2020-12-26 15:53:40 +0700

    license check

--- maintainers/scripts/package-checks.nix
+++ maintainers/scripts/package-checks.nix
@@ -15,12 +15,22 @@ let
          else [ ]))
       pkgs);
 
+  packagesWithf = cond: f:
+    pkgs.lib.concatLists (pkgs.lib.mapAttrsToList
+      (name: pkg: safeEval
+        (if pkgs.lib.isDerivation pkg && cond name pkg
+         then safeEval (f pkg)
+         else [ ]))
+      pkgs);
+
   hasDesc = pkg: builtins.hasAttr "description" pkg.meta;
+  hasLicense = pkg: builtins.hasAttr "license" pkg.meta;
   lastChar = x: builtins.substring (builtins.stringLength x - 1) (-1) x;
   report = desc: l:
     builtins.trace (pkgs.lib.concatStringsSep "\n" l) "";
 in
 {
+  inherit pkgs report packagesWith packagesWithf;
   prefixDescCaseInsen =
     report "start with the package name in the description (case insensitive)"
       (packagesWith
@@ -33,4 +43,11 @@ in
           if hasDesc pkg && builtins.stringLength pkg.meta.description != 0
           then lastChar pkg.meta.description == "."
           else false));
-}
+  licenses =
+    report "licenses of all packages" ((packagesWithf (name: pkg: builtins.hasAttr "license" pkg.meta)) (pkg: pkgs.lib.optional (pkgs.lib.isAttrs pkg.meta.license && builtins.hasAttr "shortName" pkg.meta.license) pkg.meta.license.shortName));
+   
+} 
+
+# (packagesWithf (name: pkg: builtins.hasAttr "license" pkg.meta)) (pkg: [pkg.meta.license.shortName])
+# (packagesWith (name: pkg: builtins.hasAttr "description" pkg.meta));
+# (packagesWithf (name: pkg: builtins.hasAttr "license" pkg.meta)) (pkg: pkgs.lib.optional (pkgs.lib.isAttrs pkg.meta.license && builtins.hasAttr "shortName" pkg.meta.license) pkg.meta.license.shortName)
