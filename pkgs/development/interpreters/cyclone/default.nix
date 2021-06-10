c8036bb0e0276d51cc6fb90fb40f8189aa267a46
Ben Siraphob <bensiraphob@gmail.com>
2021-06-10 19:46:49 +0700

    cyclone-scheme: 0.21 -> 0.28.0

--- pkgs/development/interpreters/cyclone/default.nix
+++ pkgs/development/interpreters/cyclone/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchFromGitHub, libck, darwin }:
 
 let
-  version = "0.21";
+  version = "0.28.0";
   bootstrap = stdenv.mkDerivation {
     pname = "cyclone-bootstrap";
     inherit version;
@@ -10,7 +10,7 @@ let
       owner = "justinethier";
       repo = "cyclone-bootstrap";
       rev = "v${version}";
-      sha256 = "0bb3a7x7vzmdyhm4nilm8bcn4q50pwqryggnxz21n16v6xakwjmr";
+      sha256 = "sha256-gZYf1Y48HrH2UVMsrPKN0IcgngweMpn/aO7x70AzD7U=";
     };
 
     enableParallelBuilding = true;
@@ -30,7 +30,7 @@ stdenv.mkDerivation {
     owner = "justinethier";
     repo = "cyclone";
     rev = "v${version}";
-    sha256 = "1vb4yaprs2bwbxmxx2zkqvysxx8r9qww2q1nqkz8yps3ji715jw7";
+    sha256 = "1vb4yaprs2bwbxmxx2zkqvysxx8r9qww2q1nq0z8yps3ji715jw7";
   };
 
   enableParallelBuilding = true;
@@ -47,5 +47,6 @@ stdenv.mkDerivation {
     description = "A brand-new compiler that allows practical application development using R7RS Scheme";
     license = licenses.mit;
     maintainers = with maintainers; [ siraben ];
+    platforms = platforms.all;
   };
 }
