85d6440d1833e9f022aadf248c5e31359698a2d6
Ben Siraphob <bensiraphob@gmail.com>
2021-06-20 20:15:04 +0700

    wip2

--- pkgs/development/python-modules/yolov4/default.nix
+++ pkgs/development/python-modules/yolov4/default.nix
@@ -4,10 +4,11 @@ buildPythonPackage rec {
   pname = "yolov4";
   version = "3.2.0";
 
-  src = fetchPypi {
-    pname = "tensorflow-yolov4";
-    version = "v${version}";
-    sha256 = "818aa57667603810415dc203ba3f75f1541e931a8dc30b6e8b21563541a70388";
+  src = fetchFromGitHub {
+    owner = "hhk7734";
+    repo = "tensorflow-yolov4";
+    rev = "v${version}";
+    sha256 = "818aa57667603810415dc203ba3f75f1541e931a8dc30b6e8b21560541a70388";
   };
 
   buildInputs = [ pytest ];
