9f68bf699d95ba9c20fc43068c27f126e3a0c6a5
Ben Siraphob <bensiraphob@gmail.com>
2021-07-12 22:12:25 +0700

    coqPackages.hierarchy-builder: 1.0.0 -> 1.1.0

--- pkgs/development/coq-modules/hierarchy-builder/default.nix
+++ pkgs/development/coq-modules/hierarchy-builder/default.nix
@@ -5,22 +5,26 @@ with lib; mkCoqDerivation {
   owner = "math-comp";
   inherit version;
   defaultVersion = with versions; switch coq.coq-version [
-    { case = isGe "8.12";         out = "1.0.0"; }
+    { case = isGe "8.12";         out = "1.1.0"; }
     { case = range "8.11" "8.12"; out = "0.10.0"; }
   ] null;
+  release."1.1.0".sha256  = "sha256-spno5ty4kU4WWiOfzoqbXF8lWlNSlySWcRReR3zE/4Q=";
   release."1.0.0".sha256  = "0yykygs0z6fby6vkiaiv3azy1i9yx4rqg8xdlgkwnf2284hffzpp";
   release."0.10.0".sha256 = "1a3vry9nzavrlrdlq3cys3f8kpq3bz447q8c4c7lh2qal61wb32h";
   releaseRev = v: "v${v}";
 
   nativeBuildInputs = [ which ];
 
-  propagatedBuildInputs = [ coq-elpi ];
+  propagatedBuildInputs = [ coq-elpi coq.ocaml ];
 
+  mlPlugin = true;
+
+  installFlags = [ "DESTDIR=$(out)" "COQMF_COQLIB=lib/coq/${coq.coq-version}" ];
   extraInstallFlags = [ "VFILES=structures.v" ];
 
   meta = {
-    description = "Coq plugin embedding ELPI.";
-    maintainers = [ maintainers.cohencyril ];
-    license = licenses.lgpl21;
+    description = "High level commands to declare a hierarchy based on packed classes";
+    maintainers = with maintainers; [ cohencyril siraben ];
+    license = licenses.mit;
   };
 }
