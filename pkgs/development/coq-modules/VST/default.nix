26a42c4dab71744c900e228667929fba0bfd9a80
Ben Siraphob <bensiraphob@gmail.com>
2021-07-22 02:04:19 +0700

    foo

--- pkgs/development/coq-modules/VST/default.nix
+++ pkgs/development/coq-modules/VST/default.nix
@@ -1,4 +1,4 @@
-{ lib, mkCoqDerivation, coq, compcert, version ? null }:
+{ lib, mkCoqDerivation, fetchFromGitHub, coq, compcert, version ? null }:
 
 with lib; mkCoqDerivation rec {
   pname = "coq${coq.coq-version}-VST";
@@ -6,17 +6,17 @@ with lib; mkCoqDerivation rec {
   displayVersion = { coq = false; };
   owner = "PrincetonUniversity";
   repo = "VST";
-  fetcher = fetchFromGitHub {
+  src = fetchFromGitHub {
     inherit owner repo;
-    rev = releaseRev;
-    sha256 = release."${version}".sha256;
+    rev = releaseRev defaultVersion;
+    sha256 = release."${defaultVersion}".sha256;
     fetchSubmodules = true;
   };
   inherit version;
   defaultVersion = with versions; switch coq.coq-version [
     { case = range "8.12" "8.13"; out = "2.8"; }
   ] null;
-  release."2.8".sha256 = "sha256-cyK88uzorRfjapNQ6XgQE0lbWnDsiyLve5po1VG52q0=";
+  release."2.8".sha256 = "sha256-6feVwb+/eLPr+ZmkRrw7OrBgV6lO3ZQBCXCnW0gUJrI=";
   releaseRev = v: "v${v}";
   propagatedBuildInputs = [ compcert ];
 
