db272ca3dab1f9c457ea3fb731866e31cd77210b
Ben Siraphob <bensiraphob@gmail.com>
2022-04-09 22:33:41 -0500

    formula: update to 2022-03-31

--- pkgs/applications/science/logic/formula/default.nix
+++ pkgs/applications/science/logic/formula/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, buildDotnetModule, dotnetCorePackages }:
+{ lib, stdenv, fetchFromGitHub, buildDotnetModule, dotnetCorePackages, unstableGitUpdater }:
 
 buildDotnetModule rec {
   pname = "formula-dotnet";
@@ -7,20 +7,24 @@ buildDotnetModule rec {
   src = fetchFromGitHub {
     owner = "VUISIS";
     repo = "formula-dotnet";
-    rev = "e962438022350dca64335c0603c00d44cb10b528";
-    sha256 = "sha256-hVtwV1MdsXaN6ZrGW4RG2HcNcv/hys/5VxGjH9vFdRE=";
+    rev = "26cb5172c64532720b6c5954ad64b8a57eca4b84";
+    sha256 = "sha256-4TTiSSt+RrgQutFENc6eeYwLgs63obG+XgnCAeh4lwo=";
   };
 
   nugetDeps = ./nuget.nix;
   projectFile = "Src/CommandLine/CommandLine.csproj";
 
+  dotnet-runtime = dotnetCorePackages.runtime_6_0;
+  dotnet-sdk = dotnetCorePackages.sdk_6_0;
   postFixup = if stdenv.isLinux then ''
     mv $out/bin/CommandLine $out/bin/formula
   '' else lib.optionalString stdenv.isDarwin ''
-    makeWrapper ${dotnetCorePackages.runtime_5_0}/bin/dotnet $out/bin/formula \
+    makeWrapper ${dotnetCorePackages.runtime_6_0}/bin/dotnet $out/bin/formula \
       --add-flags "$out/lib/formula-dotnet/CommandLine.dll" \
       --prefix DYLD_LIBRARY_PATH : $out/lib/formula-dotnet/runtimes/macos/native
   '';
+
+  passthru.updateScript = unstableGitUpdater { url = meta.homepage; };
 
   meta = with lib; {
     description = "Formal Specifications for Verification and Synthesis";
