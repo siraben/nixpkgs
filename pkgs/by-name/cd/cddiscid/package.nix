0d40327982c7efa0c021862e422e714b458ba567
Ben Siraphob <bensiraphob@gmail.com>
2025-11-17 04:29:09 -0500

    cddiscid: fix cross

--- pkgs/by-name/cd/cddiscid/package.nix
+++ pkgs/by-name/cd/cddiscid/package.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
     sha256 = "0qrcvn7227qaayjcd5rm7z0k5q89qfy5qkdgwr5pd7ih0va8rmpz";
   };
 
+  makeFlags = [ "STRIP=${stdenv.cc.targetPrefix}strip" ];
+
   installFlags = [
     "PREFIX=$(out)"
     "INSTALL=install"
