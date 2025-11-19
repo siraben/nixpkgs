64cb3b2274eb75aa2d6c0cda5a945bf810f90d37
Ben Siraphob <bensiraphob@gmail.com>
2025-11-18 17:50:04 -0800

    Update libxslt source URL to HTTPS
    Changed libxslt source from ftp://xmlsoft.org to https://download.gnome.org
    as the FTP server is no longer accessible and the bootstrap curl doesn't
    support HTTPS properly. The GNOME mirror provides the same tarball.

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -5,7 +5,7 @@ assert libxml2 != null;
 stdenv.mkDerivation {
   name = "libxslt-1.1.22";
   src = fetchurl {
-    url = ftp://xmlsoft.org/libxml2/libxslt-1.1.22.tar.gz;
+    url = https://download.gnome.org/sources/libxslt/1.1/libxslt-1.1.22.tar.gz;
     sha256 = "1nj9pvn4ibhwxpl3ry9n6d7jahppcnqc7mi87nld4vsr2vp3j7sf";
   };
   buildInputs = [libxml2];
