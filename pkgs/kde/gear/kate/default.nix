ec4aefda8a0e8426b73c648c31c76a13874bb7a0
Ben Siraphob <bensiraphob@gmail.com>
2026-08-07 00:18:07 -0700

    kate: fix build reproducibility

--- pkgs/kde/gear/kate/default.nix
+++ pkgs/kde/gear/kate/default.nix
@@ -1,4 +1,12 @@
-{ mkKdeDerivation }:
+{
+  lib,
+  mkKdeDerivation,
+}:
 mkKdeDerivation {
   pname = "kate";
+
+  # GCC produces nondeterministic DWARF when reusing precompiled headers,
+  # which also makes the build IDs of the stripped binaries differ.
+  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=124811
+  extraCmakeFlags = [ (lib.cmakeBool "ENABLE_PCH" false) ];
 }
