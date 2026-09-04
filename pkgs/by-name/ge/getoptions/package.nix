6f3afc059c362f48f97e375e99874c166ae9d376
Ben Siraphob <bensiraphob@gmail.com>
2026-09-03 21:44:53 -0700

    getoptions: modernize

--- pkgs/by-name/ge/getoptions/package.nix
+++ pkgs/by-name/ge/getoptions/package.nix
@@ -10,14 +10,17 @@
   zsh,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "getoptions";
   version = "3.3.2";
+
+  strictDeps = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "ko1nksm";
     repo = "getoptions";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-hapOGPibqt2Mm6k73v63gHxrX+lifZ8xcwzj8vWbtgo=";
   };
 
@@ -37,11 +40,13 @@ stdenvNoCC.mkDerivation rec {
   # Disable checks against yash, since shellspec seems to be broken for yash>=2.54
   # (see: https://github.com/NixOS/nixpkgs/pull/218264#pullrequestreview-1434402054)
   preCheck = ''
-    sed -i '/shellspec -s posh/d' Makefile
-    sed -i '/shellspec -s yash/d' Makefile
+    substituteInPlace Makefile \
+      --replace-fail "shellspec -s posh" "true" \
+      --replace-fail "shellspec -s yash" "true"
   ''
   + lib.strings.optionalString stdenvNoCC.hostPlatform.isDarwin ''
-    sed -i "/shellspec -s 'busybox ash'/d" Makefile
+    substituteInPlace Makefile \
+      --replace-fail "shellspec -s 'busybox ash'" "true"
   '';
 
   checkTarget = "test_in_various_shells";
@@ -49,8 +54,10 @@ stdenvNoCC.mkDerivation rec {
   meta = {
     description = "Elegant option/argument parser for shell scripts (full support for bash and all POSIX shells)";
     homepage = "https://github.com/ko1nksm/getoptions";
+    changelog = "https://github.com/ko1nksm/getoptions/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.cc0;
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ matrss ];
+    mainProgram = "getoptions";
   };
-}
+})
