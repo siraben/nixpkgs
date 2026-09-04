40c4edbaedd24df70ecc3ca339a7a36ada9e4ad0
Ben Siraphob <bensiraphob@gmail.com>
2026-09-03 21:44:02 -0700

    zsh-system-clipboard: modernize

--- pkgs/by-name/zs/zsh-system-clipboard/package.nix
+++ pkgs/by-name/zs/zsh-system-clipboard/package.nix
@@ -4,24 +4,32 @@
   fetchFromGitHub,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "zsh-system-clipboard";
   version = "0.8.0";
+
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "kutsan";
     repo = "zsh-system-clipboard";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-VWTEJGudlQlNwLOUfpo0fvh0MyA2DqV+aieNPx/WzSI=";
   };
 
   strictDeps = true;
+
   installPhase = ''
-    install -D zsh-system-clipboard.zsh $out/share/zsh/${pname}/zsh-system-clipboard.zsh
+    runHook preInstall
+
+    install -D zsh-system-clipboard.zsh \
+      $out/share/zsh/${finalAttrs.pname}/zsh-system-clipboard.zsh
+
+    runHook postInstall
   '';
 
   meta = {
-    homepage = "https://github.com/kutsan/zsh-system-clipboard";
+    homepage = finalAttrs.src.meta.homepage;
     description = "Plugin that adds key bindings support for ZLE (Zsh Line Editor) clipboard operations for vi emulation keymaps";
     license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [
@@ -30,4 +38,4 @@ stdenv.mkDerivation rec {
     ];
     platforms = lib.platforms.all;
   };
-}
+})
