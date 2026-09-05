32805cc58120ef2dffb6c2f24974eb9243a8c59c
Ben Siraphob <bensiraphob@gmail.com>
2026-09-04 22:29:56 -0700

    dump: 0.4b51 -> 0.4b55

--- pkgs/by-name/du/dump/package.nix
+++ pkgs/by-name/du/dump/package.nix
@@ -7,25 +7,31 @@
   fetchurl,
   pkg-config,
   e2fsprogs,
+  libuuid,
   ncurses,
   readline,
+  zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "dump";
-  version = "0.4b51";
+  version = "0.4b55";
 
   src = fetchurl {
     url = "mirror://sourceforge/dump/dump-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-huaDpzNVNMkVzwpQUlPay/RrYiSnD79or3RgsWPkU+s=";
+    sha256 = "sha256-FBfBFrzHKgIE+O5WycnXfbw5tn1ML2j753/51qgeEwM=";
   };
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
     e2fsprogs
+    libuuid
     ncurses
     readline
+    zlib
   ];
+
+  configureFlags = [ "--disable-werror" ];
 
   meta = {
     homepage = "https://dump.sourceforge.io/";
