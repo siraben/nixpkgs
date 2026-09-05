8709eaeb1f47e338ff1e250db1f0afa4455eee4e
Ben Siraphob <bensiraphob@gmail.com>
2026-09-05 09:47:15 +0000

    nudoku: 5.0.0 -> 8.0.1

--- pkgs/by-name/nu/nudoku/package.nix
+++ pkgs/by-name/nu/nudoku/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch2,
   autoreconfHook,
   pkg-config,
   gettext,
@@ -11,21 +10,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nudoku";
-  version = "5.0.0";
+  version = "8.0.1";
 
   src = fetchFromGitHub {
     owner = "jubalh";
     repo = "nudoku";
     rev = finalAttrs.version;
-    hash = "sha256-aOtP23kNd15DdV6on7o80QnEf0CiUBubHfFE8M1mhg0=";
+    hash = "sha256-8xSvW+oNMfPMIDTGLp6okGd1lzu/ZmZI4eYZBFhm45I=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/jubalh/nudoku/commit/c5fd9156d2d1f2c95a5d87b07adeaee90a7f911d.patch?full_index=1";
-      hash = "sha256-nvLKAUCDY3fQWjybVX8DaMBdk5CaegRRb/ENTI9kJ9k=";
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
