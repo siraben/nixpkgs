c8a06731217d81c0b46121a7c27182775c5d802d
Ben Siraphob <bensiraphob@gmail.com>
2026-09-05 07:32:13 +0000

    stalonetray: 0.8.5 -> 1.5.0

--- pkgs/by-name/st/stalonetray/package.nix
+++ pkgs/by-name/st/stalonetray/package.nix
@@ -1,50 +1,68 @@
 {
-  autoreconfHook,
+  cmocka,
   docbook_xml_dtd_44,
   docbook-xsl-ns,
   fetchFromGitHub,
   lib,
   libx11,
+  libxinerama,
   libxpm,
+  libxrandr,
   libxslt,
+  meson,
+  ninja,
+  pkg-config,
   stdenv,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "stalonetray";
-  version = "0.8.5";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "d3adb5";
     repo = "stalonetray";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-/55oP6xA1LeLawOBkhh9acaDcObO4L4ojcy7e3vwnBw=";
+    tag = finalAttrs.version;
+    hash = "sha256-8qf9WFvYdvBnX+Eca8vkAMDYkmEra9B9q+RkRhIXkX4=";
   };
 
-  preConfigure =
-    let
-      db_root = "${docbook-xsl-ns}/share/xml/docbook-xsl-ns";
-      ac_str = "AC_SUBST(DOCBOOK_ROOT)";
-      ac_str_sub = "DOCBOOK_ROOT=${db_root}; ${ac_str}";
-    in
-    ''
-      substituteInPlace configure.ac --replace '${ac_str}' '${ac_str_sub}'
-    '';
+  postPatch = ''
+    substituteInPlace generate-manpage.sh \
+      --replace-fail "/usr/share/xml/docbook/stylesheet/docbook-xsl-nons" \
+        "${docbook-xsl-ns}/share/xml/docbook-xsl-ns"
+  '';
 
   nativeBuildInputs = [
-    autoreconfHook
     docbook-xsl-ns
     docbook_xml_dtd_44
-    libx11
-    libxpm
     libxslt
+    meson
+    ninja
+    pkg-config
   ];
 
-  hardeningDisable = [ "format" ];
+  buildInputs = [
+    cmocka
+    libx11
+    libxinerama
+    libxpm
+    libxrandr
+  ];
+
+  mesonFlags = [
+    "-Dnative_kde=enabled"
+    "-Drandr=enabled"
+    "-Dtests=enabled"
+    "-Dxinerama=enabled"
+    "-Dxpm=enabled"
+  ];
+
+  doCheck = true;
 
   meta = {
     description = "Stand alone tray";
     homepage = "https://github.com/d3adb5/stalonetray";
+    changelog = "https://github.com/d3adb5/stalonetray/releases/tag/${finalAttrs.version}";
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ raskin ];
