81019a0413575602579961979b1c17563a71eea9
Ben Siraphob <bensiraphob@gmail.com>
2026-09-05 07:21:59 +0000

    console-setup: 1.242 -> 1.249

--- pkgs/by-name/co/console-setup/package.nix
+++ pkgs/by-name/co/console-setup/package.nix
@@ -12,14 +12,14 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "console-setup";
-  version = "1.242";
+  version = "1.249";
 
   src = fetchFromGitLab {
     domain = "salsa.debian.org";
     owner = "installer-team";
     repo = "console-setup";
     tag = finalAttrs.version;
-    hash = "sha256-5PV1Mbg7ZGQsotwnBVz8DI77Y8ULCnoTANqBLlP3YrE=";
+    hash = "sha256-ikRL93IOtUQF6YhrgojGwCNKFl6iN31GRTlne7527ng=";
   };
 
   buildInputs = [
