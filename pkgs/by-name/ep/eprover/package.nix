e28881d74d2251a0ffbea02f60eb98c418c15852
Ben Siraphob <bensiraphob@gmail.com>
2026-09-04 22:07:08 -0700

    eprover: 3.2 -> 3.5.1

--- pkgs/by-name/ep/eprover/package.nix
+++ pkgs/by-name/ep/eprover/package.nix
@@ -1,18 +1,20 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   which,
   enableHO ? false,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "eprover";
-  version = "3.2";
+  version = "3.5.1";
 
-  src = fetchurl {
-    url = "https://wwwlehre.dhbw-stuttgart.de/~sschulz/WORK/E_DOWNLOAD/V_${finalAttrs.version}/E.tgz";
-    hash = "sha256-B0yOX8MGJHY0HOeQ/RWtgATTIta2YnhEvSdoqIML1K4=";
+  src = fetchFromGitHub {
+    owner = "eprover";
+    repo = "eprover";
+    tag = "E-${finalAttrs.version}";
+    hash = "sha256-zHcU3WQx3HTKKCGjxJTVulqToIEofZiJs7bmD3Ktgms=";
   };
 
   buildInputs = [ which ];
@@ -41,7 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Automated theorem prover for full first-order logic with equality";
-    homepage = "http://www.eprover.org/";
+    homepage = "https://www.eprover.org/";
     license = lib.licenses.gpl2;
     maintainers = with lib.maintainers; [
       raskin
